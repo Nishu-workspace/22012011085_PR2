6bf9164036337c2dd68b04f23a075d366d6a6693
Flare <82433193+Flare-workspace@users.noreply.github.com>
2024-08-18 19:50:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a22012011085_pr2/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.a22012011085_pr2
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MainAcitivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showMessage("onStat method is called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        showMessage("onPause method is called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showMessage("onStop method is called")
+    }
+    override fun onResume() {
+        super.onResume()
+        showMessage("onResume method is called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        showMessage("onRestart method is called")
+    }
+    fun showMessage(msg:String){
+        Log.i(TAG,msg)
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+    }
+}
