bdcd3e11c3bca7e7ea3480fcc5415a6960f87c9c
Flare <82433193+Flare-workspace@users.noreply.github.com>
2024-09-25 21:04:15 +0530

    Added log and snackbar message

--- app/src/main/java/com/example/a22012011085_pr2/MainActivity.kt
+++ app/src/main/java/com/example/a22012011085_pr2/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainAcitivity"
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        showSnackbarMessage("onCreate Method is called")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        showMessage("onStat method is called")
+        showMessage("onStart method is called")
     }
 
     override fun onPause() {
@@ -47,5 +49,9 @@ class MainActivity : AppCompatActivity() {
     fun showMessage(msg:String){
         Log.i(TAG,msg)
         Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+    }
+    fun showSnackbarMessage(msg:String){
+        Log.i(TAG,msg)
+        Snackbar.make(findViewById(R.id.main),msg,Snackbar.LENGTH_SHORT).show()
     }
 }
